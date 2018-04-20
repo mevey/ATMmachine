63a0be8bc07bfdd0eab69801d7b7a08b60daaf6f
mevey <muthoni90@gmail.com>
2018-04-19 22:23:57 -0700

    first commit

---
+++ src/BankAccount.java
@@ -0,0 +1,11 @@
+public class BankAccount {
+    private final double balance;
+
+    public BankAccount() {
+        this.balance=0.0;
+    }
+
+    public double balance() {
+        return 0.0;
+    }
+}
---
+++ test/BankAccountTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class BankAccountTest {
+    @Test
+    public void aNewBankAccountShouldHaveZeroBalance() {
+        BankAccount a = new BankAccount();
+        assertEquals(a.balance(), 0.0);
+    }
+}
