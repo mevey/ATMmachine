3aaa85e263bad9d865c0004dbbd56ba01cde62c2
mevey <muthoni90@gmail.com>
2018-04-19 22:43:09 -0700

    simple withdrawal

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 public class BankAccount {
+    private static final double WITHDRAWAL_FEE = 0.5;
     private double balance;
     private final ArrayList<Object> transactions;
 
@@ -16,6 +17,13 @@ public class BankAccount {
     public double makeDeposit(Double amount) {
         this.balance += amount;
         this.transactions.add(amount);
+        return this.balance;
+    }
+
+    public double withdraw(double amount) {
+        this.balance -= (amount + WITHDRAWAL_FEE);
+        this.transactions.add(amount * -1);
+        this.transactions.add(WITHDRAWAL_FEE);
         return this.balance;
     }
 }
